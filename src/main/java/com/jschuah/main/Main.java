5846a33cfa2467daf687ba531b8f38cba73eb1d7
Chuah Jun Sheng <chuahjunsheng@gmail.com>
2017-04-21 19:37:04 +0800

    commit

--- src/main/java/com/jschuah/main/Main.java
+++ src/main/java/com/jschuah/main/Main.java
@@ -7,6 +7,6 @@ import static spark.Spark.get;
  */
 public class Main {
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello World 3");
+        get("/", (req, res) -> "Hello World 4");
     }
 }
